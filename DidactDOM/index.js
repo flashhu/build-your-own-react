39d5214d89d031715df08a5ced7b295e9a746700
flashhu <flashhu@163.com>
2020-07-19 16:23:32 +0800

    配置 Babel

--- DidactDOM/index.js
+++ DidactDOM/index.js
@@ -1,18 +1,203 @@
-function render(element, container) {
+function createDom(fiber) {
     const dom =
-        element.type === "TEXT_ELEMENT"
+        fiber.type === "TEXT_ELEMENT"
             ? document.createTextNode("")
-            : document.createElement(element.type);
-    const isProperty = key => key != "children";
-    Object.keys(element.props)
+            : document.createElement(fiber.type);
+    Object.keys(fiber.props)
         .filter(isProperty)
         .forEach(name => {
-            dom[name] = element.props[name]
+            dom[name] = fiber.props[name]
         })
-    element.props.children.forEach(child => {
-        render(child, dom);
-    });
-    container.appendChild(dom);
+    return dom;
+}
+
+const isEvent = key => key.startsWith("on")
+const isProperty = key => key != "children" && !isEvent(key);
+const isNew = (prev, next) => key => prev[key] !== next[key];
+const isGone = (prev, next) => key => !(key in next)
+function updateDom(dom, prevProps, nextProps) {
+    //remove old or changed event listeners
+    Object.keys(prevProps)
+        .filter(isEvent)
+        .filter(key => !(key in nextProps) || isNew(prevProps, nextProps)(key))
+        .forEach(name => {
+            const eventType = name.toLowerCase().substring(2);
+            dom.removeEventListener(
+                eventType,
+                prevProps[name]
+            )
+        })
+
+    //add event listeners
+    Object.keys(nextProps)
+        .filter(isEvent)
+        .filter(isNew(prevProps, nextProps))
+        .forEach(name => {
+            const eventType = name.toLowerCase().substring(2);
+            dom.addEventListener(
+                eventType,
+                nextProps[name]
+            )
+        })
+
+    // remove old properties
+    Object.keys(prevProps)
+        .filter(isProperty)
+        .filter(isGone(prevProps, nextProps))
+        .forEach(name => {
+            dom[name] = ""
+        })
+
+    // set new or changed properties
+    Object.keys(nextProps)
+        .filter(isProperty)
+        .filter(isNew(prevProps, nextProps))
+        .forEach(name => {
+            dom[name] = nextProps[name]
+        })
+}
+
+function commitRoot() {
+    deletions.forEach(commitWork)
+    commitWork(wipRoot.child)
+    currentRoot = wipRoot
+    wipRoot = null
+}
+
+function commitWork(fiber) {
+    if (!fiber) {
+        return
+    }
+    const domParent = fiber.parent.dom;
+    if (fiber.effectTag === "PLACEMENT" && fiber.dom != null) {
+        domParent.appendChild(fiber.dom);
+    } else if (fiber.effectTag === "DELETION") {
+        domParent.removeChild(fiber.dom);
+    } else if (fiber.effectTag === "UPDATE" && fiber.dom != null) {
+        updateDom(fiber.dom, fiber.alternate.props, fiber.props);
+    }
+    domParent.appendChild(fiber.dom);
+    commitWork(fiber.child);
+    commitWork(fiber.sibling);
+}
+
+function render(element, container) {
+    // root
+    wipRoot = {
+        dom: container,
+        props: {
+            children: [element]
+        },
+        alternate: currentRoot,
+    }
+    deletions = []
+    nextUnitOfWork = wipRoot
+}
+
+let nextUnitOfWork = null
+let currentRoot = null
+let wipRoot = null
+let deletions = null
+
+function workLoop(deadline) {
+    let shouldYield = false;
+    // 有工作且有剩余时间
+    while (nextUnitOfWork && !shouldYield) {
+        nextUnitOfWork = peformUnitOfWork(nextUnitOfWork);
+        shouldYield = deadline.timeRemaining() < 1;
+    }
+
+    if (!nextUnitOfWork && wipRoot) {
+        commitRoot();
+    }
+
+    // like setTimeout 在浏览器的空闲时段内调用的函数排队
+    requestIdleCallback(workLoop)
+}
+
+requestIdleCallback(workLoop)
+
+function peformUnitOfWork(fiber) {
+    // add dom node
+    if (!fiber.dom) {
+        fiber.dom = createDom(fiber)
+    }
+
+    // create new fibers
+    const elements = fiber.props.children;
+    reconcileChildren(fiber, elements);
+
+    // return next unit of work
+    // 1. child node
+    if (fiber.child) {
+        return fiber.child;
+    }
+    let nextFiber = fiber;
+    while (nextFiber) {
+        // 2. sibling node
+        if (nextFiber.sibling) {
+            return nextFiber.sibling;
+        }
+        // 3. sibling of the parent
+        nextFiber = nextFiber.parent
+    }
+}
+
+function reconcileChildren(wipFiber, elements) {
+    let index = 0;
+    let oldFiber = wipFiber.alternate && wipFiber.alternate.child;
+    let prevSibling = null; //之前的兄弟节点
+
+    while (index < elements.length || oldFiber != null) {
+        const element = elements[index];
+        let newFiber = null;
+
+        //compare oldFiber to element
+        const sameType = oldFiber && element && element.type == oldFiber.type;
+
+        if (sameType) {
+            //update the node
+            newFiber = {
+                type: oldFiber.type,
+                props: element.props,
+                dom: oldFiber.dom,
+                parent: wipFiber,
+                alternate: oldFiber,
+                effectTag: "UPDATE"
+            }
+        }
+        if (!sameType && element) {
+            //add this node
+            newFiber = {
+                type: element.type,
+                props: element.props,
+                dom: null,
+                parent: wipFiber,
+                alternate: null,
+                effectTag: "PLACEMENT"
+            }
+        }
+        if (!sameType && oldFiber) {
+            //delete the oldFiber's node
+            oldFiber.effectTag = "DELETION";
+            deletions.push(oldFiber);
+        }
+
+        if (oldFiber) {
+            oldFiber = oldFiber.sibling
+        }
+
+        // 子节点 或 兄弟节点
+        if (index === 0) {
+            wipFiber.child = newFiber;
+        } else {
+            prevSibling.sibling = newFiber;
+        }
+
+        // ?
+        prevSibling = newFiber;
+        index++;
+    }
 }
 
 const DidactDOM = {
