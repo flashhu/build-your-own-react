d92ea267afc32b5cccb1dacdfecf98c40e0a14ba
flashhu <flashhu@163.com>
2020-07-19 18:41:27 +0800

    Function Components; Hooks

--- DidactDOM/index.js
+++ DidactDOM/index.js
@@ -68,18 +68,33 @@ function commitWork(fiber) {
     if (!fiber) {
         return
     }
-    const domParent = fiber.parent.dom;
+
+    // find parent node
+    let domParentFiber = fiber.parent;
+    while(!domParentFiber.dom) {
+        domParentFiber = domParentFiber.parent;
+    }
+    const domParent = domParentFiber.dom;
+
     if (fiber.effectTag === "PLACEMENT" && fiber.dom != null) {
         domParent.appendChild(fiber.dom);
     } else if (fiber.effectTag === "DELETION") {
-        domParent.removeChild(fiber.dom);
+        commitDeletion(fiber, domParent);
     } else if (fiber.effectTag === "UPDATE" && fiber.dom != null) {
         updateDom(fiber.dom, fiber.alternate.props, fiber.props);
     }
-    domParent.appendChild(fiber.dom);
+
     commitWork(fiber.child);
     commitWork(fiber.sibling);
 }
+
+function commitDeletion(fiber, domParent) {
+    if (fiber.dom) {
+        domParent.removeChild(fiber.dom);
+    }else {
+        commitDeletion(fiber.child, domParent);
+    }
+} 
 
 function render(element, container) {
     // root
@@ -118,14 +133,13 @@ function workLoop(deadline) {
 requestIdleCallback(workLoop)
 
 function peformUnitOfWork(fiber) {
-    // add dom node
-    if (!fiber.dom) {
-        fiber.dom = createDom(fiber)
+    const isFunctionComponent = fiber.type instanceof Function;
+    if(isFunctionComponent) {
+        updateFunctionComponent(fiber);
+    }else {
+        updateHostComponent(fiber);
     }
-
-    // create new fibers
-    const elements = fiber.props.children;
-    reconcileChildren(fiber, elements);
+    
 
     // return next unit of work
     // 1. child node
@@ -141,6 +155,60 @@ function peformUnitOfWork(fiber) {
         // 3. sibling of the parent
         nextFiber = nextFiber.parent
     }
+}
+
+let wipFiber = null;
+let hookIndex = null;
+
+function updateFunctionComponent(fiber) {
+    wipFiber = fiber;
+    hookIndex = 0;
+    wipFiber.hooks = [];
+    const children = [fiber.type(fiber.props)]
+    reconcileChildren(fiber, children)
+}
+
+function useState(initial) {
+    const oldHook =
+        wipFiber.alternate &&
+        wipFiber.alternate.hooks &&
+        wipFiber.alternate.hooks[hookIndex]
+    const hook = { 
+        state: oldHook ? oldHook.state : initial,
+        queue: []
+    };
+
+    // ?
+    const actions = oldHook ? oldHook.queue : [];
+    actions.forEach(action => {
+        // ?
+        hook.state = action(hook.state)
+    })
+
+    const setState = action => {
+        hook.queue.push(action)
+        wipRoot = {
+            dom: currentRoot.dom,
+            props: currentRoot.props,
+            alternate: currentRoot
+        }
+        nextUnitOfWork = wipRoot;
+        deletions = []
+    }
+
+    wipFiber.hooks.push(hook)
+    hookIndex ++
+    return [hook.state, setState]
+}
+
+function updateHostComponent(fiber) {
+    // add dom node
+    if (!fiber.dom) {
+        fiber.dom = createDom(fiber)
+    }
+
+    // create new fibers
+    reconcileChildren(fiber, fiber.props.children);
 }
 
 function reconcileChildren(wipFiber, elements) {
@@ -201,7 +269,8 @@ function reconcileChildren(wipFiber, elements) {
 }
 
 const DidactDOM = {
-    render
+    render,
+    useState
 }
 
 export default DidactDOM
--- index.js
+++ index.js
@@ -2,12 +2,22 @@ import DidactDOM from './DidactDOM'
 import Didact from './Didact'
 
 /** @jsx Didact.createElement */
-const element = (
-    <div id="foo">
-        <a>bar</a>
-        <b />
-    </div>
-)
+function Counter() {
+    const [state, setState] = DidactDOM.useState(1)
+    return (
+        <h1 onClick={() => setState(c => c + 1)}>
+            Count: {state}
+        </h1>
+    )
+}
+
+const element = <Counter />
+// const element = (
+//     <div id="foo">
+//         <a>bar</a>
+//         <b />
+//     </div>
+// )
 
 const container = document.getElementById("root")
 DidactDOM.render(element, container)
